5f532544a1256af5b4c8e391a307d2dbe01fa790
nbhosle <naren.bhosle@gmail.com>
2020-03-20 16:11:05 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is readme file',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
 }
